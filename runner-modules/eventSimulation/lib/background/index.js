3e489b0207a8619b7c671fa266df84d777856f98
Joris van der Wel <joris@jorisvanderwel.com>
2017-11-28 17:41:37 +0100

    Report the eventSimulation commands as runResult events
    And also update the default example script

--- runner-modules/eventSimulation/lib/background/index.js
+++ runner-modules/eventSimulation/lib/background/index.js
@@ -3,6 +3,7 @@
 const scriptEnvUrl = browser.extension.getURL('/build/eventSimulation-script-env.js');
 
 module.exports = async script => {
+    await script.include('runResult'); // required by content
     const handleTabsInitializingTabContent = ({executeContentScript}) => {
         executeContentScript('eventSimulation', '/build/eventSimulation-content.js');
     };
--- runner-modules/eventSimulation/lib/content/index.js
+++ runner-modules/eventSimulation/lib/content/index.js
@@ -3,9 +3,22 @@
 const {click} = require('./mouse');
 const {focus} = require('./focus');
 
-openRunnerRegisterRunnerModule('eventSimulation', async () => {
+openRunnerRegisterRunnerModule('eventSimulation', async ({getModule}) => {
+    const {scriptResult} = await getModule('runResult');
+
     return Object.freeze({
-        click,
-        focus,
+        click: async (element, {x = null, y = null, mouseDownDuration = 64} = {}) => {
+            await scriptResult.execEvent('command:eventSimulation.click', async event => {
+                await click(element, {x, y, mouseDownDuration});
+                event.shortTitle = 'Click on element';
+                event.setMetaData('mouseDownDuration', mouseDownDuration);
+            });
+        },
+        focus: async element => {
+            await scriptResult.execEvent('command:eventSimulation.focus', event => {
+                focus(element);
+                event.shortTitle = 'Focus an element';
+            });
+        },
     });
 });
